cd2429ae9de8a3ee5019b08628f9c341617a892d
Nikhil Shinde <nikhilshinde00@outlook.com>
2022-05-30 19:23:25 +0530

    portfolio cypress

--- my-app/cypress/integration/my-own-test/hello.test.js
+++ my-app/cypress/integration/my-own-test/hello.test.js
@@ -1,35 +1,45 @@
+/* eslint-disable testing-library/await-async-utils */
+/* eslint-disable cypress/no-unnecessary-waiting */
 /* eslint-disable jest/valid-expect */
 /// <reference types="cypress" />
 
 describe("Empty",()=>{
   it("test one",()=>{
-    cy.visit("http://localhost:3000/")
+    cy.visit("https://nikhilshinde-portfolio.netlify.app/")
 
-    cy.contains("Learn Testing and Dev").should("exist")
+    cy.contains("Projects").should("exist")
 
-    cy.get("p").should(($div)=>{
+    cy.get("h1").should(($div)=>{
       
-      expect($div.get(0).innerText).contains("Edit")
+      expect($div.get(1).innerText).contains("Experience")
     })
     cy.log("adfaesf")
-    cy.log(cy.get("p"))
+    cy.log(cy.get("h1"))
     cy.log("adfaesf")
 
     cy.get("div.App-header").should("not.exist")
     cy.location('host') 
-    cy.location().should((loc) => {
-      expect(loc.hash).to.eq('')
-      expect(loc.host).to.eq('localhost:3000')
-      expect(loc.hostname).to.eq('localhost')
-      expect(loc.href).to.eq(
-        'http://localhost:3000/'
-      )
-     expect(loc.origin).to.eq('http://localhost:3000')
-      expect(loc.port).to.eq('3000')
-      expect(loc.protocol).to.eq('http:')
-    })
+    // project
+    cy.get("h1").eq(0).click()
+    cy.go('back')
 
+    //experience
+    cy.get("h1").eq(1).click()
+    cy.go('back')
 
+    // skill
+    cy.get("h1").eq(2).click()
+    cy.wait(2000);
+    cy.get(".left-div").click()
+
+    // contact
+    cy.wait(2000);
+    cy.get("h1").eq(3).click();
+    cy.get("#fname").type("Nikhil Shinde")
+    cy.get("#email").type("shinden523@gmail.com")
+    cy.get("#phone").type("8329634377");
+    cy.get("#comment").type("ksjfb;kjsnefk;jn;lksjne;gfj;kjr g;kj;aksr jg',;m s;r,gf")
+    cy.get(".back-button").click()
 
   })
 })
