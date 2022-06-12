5208630880fc8c1c72e3160fc9c003047ff5fdd1
Nikhil Shinde <nikhilshinde00@outlook.com>
2022-06-12 17:55:36 +0530

    cypress testing

--- my-app/cypress/integration/authenticate.test.js
+++ my-app/cypress/integration/authenticate.test.js
@@ -1,6 +1,3 @@
-/* eslint-disable testing-library/await-async-utils */
-/* eslint-disable cypress/no-unnecessary-waiting */
-/* eslint-disable jest/valid-expect */
 /// <reference types="cypress" />
 
 describe("Basic Authenticated Testing",()=>{
@@ -22,11 +19,52 @@ describe("Basic Authenticated Testing",()=>{
 
 
   it("Should pass",()=>{
-     cy.visit("https://codedamn.com/dashboard")
-     cy.get('[data-content="playgrounds"]').click()
-     cy.get("#playgrounds a").eq(0).click()
-     cy.get('[data-testid="create-snippet-btn"]').click()
+     cy.visit("https://codedamn.com/playground/20vgjpQgTAVcZEfTGubF6")
+    //  cy.pause()
 
+    cy.log("Checking for test in right sidebar")
+    // cy.get(".command-message-text").then((d)=>{
+    //    cy.log(d)
+    // })
+    // cy.get("").then((el)=>{
+    //   expect(el.text()).to.eg("...")
+    // })
+    // cy.get("").should("contain.text","e")'
+
+
+
+    cy.contains("Saved").should("exist")
+    cy.debug() 
+    cy.get(".xterm-screen > canvas").eq(3).then((l)=>{
+      cy.log(l)
+    })
+
+
+    // cy.contains("Drop files from your device to upload.").should("have.text")
+    // freeze your js thread
+
+  })
+
+  it.only("New program to test",()=>{
+    // 
+    cy.visit("https://codedamn.com/playground/20vgjpQgTAVcZEfTGubF6")
+    //  cy.pause()
+
+    cy.log("Checking for test in right sidebar")
+    cy.contains("Saved").should("exist")
+    cy.contains("Manage Collaborators").should("exist")
+    
+    cy.wait(1000*10).then(()=>{
+      cy.get("[data-testid=xterm]")
+      .type("{ctrl}{c}")
+      .type("touch test1.js{enter}");
+      cy.contains("test1.js").rightclick();
+
+      cy.contains("Rename File").click();
+      cy.get('[data-fullid="test1.js"]')
+      .type("{selectAll}{backspace}")
+      .type("nikhil.js{enter}")
+    })
   })
 
 
